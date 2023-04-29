1d16497117794770078fdc967f2470d3f96d76b6
Kain <kainedezz.2000@gmail.com>
2023-04-30 04:45:29 +0600

    little fix

--- front/src/core/Root.js
+++ front/src/core/Root.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Outlet} from "react-router-dom";
+import {Navigate, Outlet} from "react-router-dom";
 import Header from "./Header";
 import {Container} from "@mui/material";
 import withNavigate from "../decorators/withNavigate";
@@ -8,10 +8,7 @@ import withUser from "../decorators/withUser";
 
 class Root extends React.Component {
     render() {
-        if (this.props.user === undefined) {
-            this.props.navigate('/login?redirect=' + document.location.pathname)
-            return null
-        }
+        if (this.props.user === undefined) return <Navigate to={'/login?redirect=' + document.location.pathname}/>
 
         return (
             <>
